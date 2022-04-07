c5af18fc6db0b8a054b75000f61adefaa67b2457
ReIAm <57282194+Re14m@users.noreply.github.com>
2022-04-07 10:44:33 +0900

    Add files via upload

---
+++ 2022-04-07_recipie220/janome_analytics.py
@@ -0,0 +1,17 @@
+# ライブラリのインストール
+from janome.tokenizer import Tokenizer
+import collections
+
+# search_tweet.pyで生成したtxtを読込
+with open('search_tweets.txt', 'r', encoding='UTF-8') as f:
+    text = f.read()
+
+# 形態素解析結果をtとする
+t = Tokenizer()
+
+# tを名詞と固有名詞に分けてcとしてカウント
+c = collections.Counter(token.base_form for token in t.tokenize(text)
+                        if token.part_of_speech.startswith('名詞,固有名詞'))
+
+# カウント結果の上位30位を表示
+print(c.most_common()[:30])
